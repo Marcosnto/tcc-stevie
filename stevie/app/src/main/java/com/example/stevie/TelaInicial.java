42943bb89e7060576eda78cd737ad67e00a6101f
marcosnto <marcos_nto@hotmail.com>
2019-08-19 02:42:22 -0300

    Criação da conexão em bluetooth com o servidor no App e no Servidor.

---
+++ stevie/app/src/main/java/com/example/stevie/TelaInicial.java
@@ -0,0 +1,13 @@
+package com.example.stevie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+}
